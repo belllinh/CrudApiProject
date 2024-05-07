c1cdd87f6c8e183193201291b5bb6dba1b8cb14b
HP <HP@CUCCUNG>
2024-05-07 11:34:03 +0700

    Add project files.

---
+++ CrudApiProject/Models/ApiDemoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudApiProject.Models
+{
+    public class ApiDemoDbContext :DbContext
+    {
+        public ApiDemoDbContext(DbContextOptions<ApiDemoDbContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ CrudApiProject/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudApiProject.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string? FirstName {  get; set; }
+
+        public string? LastName {  get; set; }
+
+        public string? EmailAddress { get; set; }
+
+
+    }
+}
